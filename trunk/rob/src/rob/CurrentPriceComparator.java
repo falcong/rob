4a6229bee32119af10535e90e262300759b46869
nekomukuro <nekomukuro@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-05-16 17:52:04 +0000

    Aggiunto metodo che ordina per prezzo corrente in Problem (e relativa classe comparatore)+Test della stessa.

---
+++ trunk/rob/src/rob/CurrentPriceComparator.java
@@ -0,0 +1,31 @@
+package rob;
+
+import java.util.Comparator;
+
+public class CurrentPriceComparator implements Comparator<Supplier> {
+	Solution solution;
+	int product;
+	
+	public CurrentPriceComparator(Solution sol, int product) {
+		this.solution = sol;
+		this.product = product;
+	}
+	
+	@Override
+	public int compare(Supplier s1, Supplier s2) {
+		double priceS1 = s1.getDiscountedPrice(product, solution.getQuantity(s1.getId(), product));
+		double priceS2 = s2.getDiscountedPrice(product, solution.getQuantity(s2.getId(), product));
+		
+		if (priceS1<priceS2)
+			return -1;
+		else if (priceS1==priceS2)
+			return 0;
+		else
+			return 1;
+	}
+	
+	public void setSolution(Solution sol){
+		this.solution=sol;
+		}
+
+}

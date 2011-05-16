89c4c9333aaf5ae1427a7629f18118f87855027b
nekomukuro <nekomukuro@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-05-16 20:35:31 +0000

    BoughtQuantityComparator

---
+++ trunk/rob/src/rob/BoughtQuantityComparator.java
@@ -0,0 +1,24 @@
+package rob;
+
+import java.util.Comparator;
+
+public class BoughtQuantityComparator implements Comparator<Supplier> {
+	
+	Solution solution;
+	
+	public BoughtQuantityComparator(Solution sol) {
+		this.solution = sol;
+	}
+	@Override
+	public int compare(Supplier s1, Supplier s2) {
+		int totalS1=solution.totalQuantityBought(s1.getId());
+		int totalS2=solution.totalQuantityBought(s2.getId());
+		if(totalS1<totalS2)
+			return -1;
+		else if (totalS1==totalS2)
+			return 0;
+		else
+			return 1;
+	}
+
+}

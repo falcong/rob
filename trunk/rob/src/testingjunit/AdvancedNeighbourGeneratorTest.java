4cc4444e08c35f50a9c07281b3e068f031de2098
nekomukuro <nekomukuro@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-05-24 20:03:57 +0000

    PULIZIA CODICE - Eliminata la vecchia classe AdvancedNeighbourGenerator (non usata perchè lenta e buggata), sostituita con AdvancedNeighbourGenerator2 (ora AdvancedNeighbourGenerator) - Eliminato il parser dei runfile e relativi test - Eliminate le classi Solver e Run (e relativi test) perché non più usate - Copiato Statistiche5 in main() (testato e funzionante)

--- trunk/rob/src/testingjunit/AdvancedNeighbourGeneratorTest.java
+++ trunk/rob/src/testingjunit/AdvancedNeighbourGeneratorTest.java
@@ -10,82 +10,85 @@ import rob.Solution;
 import rob.Supplier;
 import rob.Utility;
 import solutionhandlers.AdvancedNeighbourGenerator;
+import solutionhandlers.LinesSolutionGenerator;
+import solutionhandlers.SolutionGenerator;
+import solutionhandlers.TrivialSolutionGenerator;
 
 
 public class AdvancedNeighbourGeneratorTest {
-	@Test
-	public final void testFindSupplierImproveablePublic(){
-		/*
-		 * problema1.txt
-		 * soluzione tale che
-		 * s1 -> fascia di sconto 2 (massima)
-		 * s2 -> fascia di sconto 1
-		 */
-		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
-		Problem problem = pp.parse("problema1.txt");
-		int [] s0={0, 0, 0, 0};
-		int [] s1={0, 51, 42, 53};
-		int [] s2={0, 9, 0, 10};
-		int [][] matrix=new int[3][];
-		matrix[0]=s0;
-		matrix[1]=s1;
-		matrix[2]=s2;
-		Solution solution=new Solution(matrix,problem);
-		assertTrue(solution.isAdmissible(problem));
-		
-		AdvancedNeighbourGenerator generator = new AdvancedNeighbourGenerator(problem);
-		assertEquals(2, generator.findSupplierImproveablePublic(solution));
-	}
-	
-	@Test
-	public final void testFindSupplierNotSaturatedPublic(){
-		/*
-		 * problema1.txt
-		 * soluzione tale che
-		 * s1 -> fascia di sconto 2 (massima)
-		 * s2 -> fascia di sconto 3 (massima) + saturazione
-		 */
-		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
-		Problem problem = pp.parse("problema1.txt");
-		int [] s0={0, 0, 0, 0};
-		int [] s1={0, 23, 42, 24};
-		int [] s2={0, 37, 0, 39};
-		int [][] matrix=new int[3][];
-		matrix[0]=s0;
-		matrix[1]=s1;
-		matrix[2]=s2;
-		Solution solution=new Solution(matrix,problem);
-		assertTrue(solution.isAdmissible(problem));
-		
-		AdvancedNeighbourGenerator generator = new AdvancedNeighbourGenerator(problem);
-		assertEquals(1, generator.findSupplierNotSaturatedPublic(solution));
-	}
-	
-	
-	@Test
-	public final void testFindTargetSupplierPublic(){
-		/*
-		 * problema1.txt
-		 * soluzione tale che
-		 * s1 -> fascia di sconto 2 (massima)
-		 * s2 -> fascia di sconto 3 (massima) + saturazione
-		 */
-		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
-		Problem problem = pp.parse("problema1.txt");
-		int [] s0={0, 0, 0, 0};
-		int [] s1={0, 23, 42, 24};
-		int [] s2={0, 37, 0, 39};
-		int [][] matrix=new int[3][];
-		matrix[0]=s0;
-		matrix[1]=s1;
-		matrix[2]=s2;
-		Solution solution=new Solution(matrix,problem);
-		assertTrue(solution.isAdmissible(problem));
-		
-		AdvancedNeighbourGenerator generator = new AdvancedNeighbourGenerator(problem);
-		assertEquals(1, generator.findTargetSupplierPublic(solution));
-	}
-	
+//	@Test
+//	public final void testFindSupplierImproveablePublic(){
+//		/*
+//		 * problema1.txt
+//		 * soluzione tale che
+//		 * s1 -> fascia di sconto 2 (massima)
+//		 * s2 -> fascia di sconto 1
+//		 */
+//		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
+//		Problem problem = pp.parse("problema1.txt");
+//		int [] s0={0, 0, 0, 0};
+//		int [] s1={0, 51, 42, 53};
+//		int [] s2={0, 9, 0, 10};
+//		int [][] matrix=new int[3][];
+//		matrix[0]=s0;
+//		matrix[1]=s1;
+//		matrix[2]=s2;
+//		Solution solution=new Solution(matrix,problem);
+//		assertTrue(solution.isAdmissible(problem));
+//		
+//		AdvancedNeighbourGenerator2 generator = new AdvancedNeighbourGenerator2(problem);
+//		assertEquals(2, generator.findSupplierImproveablePublic(solution));
+//	}
+//	
+//	@Test
+//	public final void testFindSupplierNotSaturatedPublic(){
+//		/*
+//		 * problema1.txt
+//		 * soluzione tale che
+//		 * s1 -> fascia di sconto 2 (massima)
+//		 * s2 -> fascia di sconto 3 (massima) + saturazione
+//		 */
+//		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
+//		Problem problem = pp.parse("problema1.txt");
+//		int [] s0={0, 0, 0, 0};
+//		int [] s1={0, 23, 42, 24};
+//		int [] s2={0, 37, 0, 39};
+//		int [][] matrix=new int[3][];
+//		matrix[0]=s0;
+//		matrix[1]=s1;
+//		matrix[2]=s2;
+//		Solution solution=new Solution(matrix,problem);
+//		assertTrue(solution.isAdmissible(problem));
+//		
+//		AdvancedNeighbourGenerator2 generator = new AdvancedNeighbourGenerator2(problem);
+//		assertEquals(1, generator.findSupplierNotSaturatedPublic(solution));
+//	}
+//	
+//	
+//	@Test
+//	public final void testFindTargetSupplierPublic(){
+//		/*
+//		 * problema1.txt
+//		 * soluzione tale che
+//		 * s1 -> fascia di sconto 2 (massima)
+//		 * s2 -> fascia di sconto 3 (massima) + saturazione
+//		 */
+//		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
+//		Problem problem = pp.parse("problema1.txt");
+//		int [] s0={0, 0, 0, 0};
+//		int [] s1={0, 23, 42, 24};
+//		int [] s2={0, 37, 0, 39};
+//		int [][] matrix=new int[3][];
+//		matrix[0]=s0;
+//		matrix[1]=s1;
+//		matrix[2]=s2;
+//		Solution solution=new Solution(matrix,problem);
+//		assertTrue(solution.isAdmissible(problem));
+//		
+//		AdvancedNeighbourGenerator2 generator = new AdvancedNeighbourGenerator2(problem);
+//		assertEquals(1, generator.findTargetSupplierPublic(solution));
+//	}
+//	
 	@Test
 	public final void testGenerate(){
 		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
@@ -123,6 +126,21 @@ public class AdvancedNeighbourGeneratorTest {
 				"fascia s1 = "+sup1Segment2+"\n"+
 				"fascia s2 = "+sup2Segment2);
 		//solution2.print();
-		assertTrue(sup1Segment2==sup1Segment1+1 || sup2Segment2==sup2Segment1+1);
+		assertTrue(sup1Segment2==sup1Segment1+1);
 	}
+	
+	@Test
+	public final void testGenerate2(){
+		ProblemParser parser = new ProblemParser(Utility.getConfigParameter("problemsPath"));
+		//62
+		Problem problem = parser.parse("Cap.100.100.5.1.10.1.ctqd");
+		SolutionGenerator gen = new TrivialSolutionGenerator(problem);
+		Solution tSol = gen.generate();
+		System.out.println("fo trivial="+tSol.getObjectiveFunction());
+		AdvancedNeighbourGenerator nGen = new AdvancedNeighbourGenerator(problem);
+		
+		Solution neighbour=nGen.generate(tSol, 1);
+		System.out.println("fo vicino="+neighbour.getObjectiveFunction());
+		assertTrue(neighbour.isAdmissible(problem));
+		}
 }

95161a787ea2f1c62b2e79dc9169b23aa490556e
claudio.landi24@gmail.com <claudio.landi24@gmail.com@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-07-26 15:58:40 +0000

    realizzato testing fino a CurrentPriceComparator

--- trunk/rob/src/testingjunit/CurrentPriceComparatorTest.java
+++ trunk/rob/src/testingjunit/CurrentPriceComparatorTest.java
@@ -14,19 +14,18 @@ import rob.Supplier;
 public class CurrentPriceComparatorTest {
 	ProblemParser pp = new ProblemParser(Constants.INPUT_PATH);
 	
-	final String PROBLEM_NAME = "problema4.txt";
-	Problem problem = pp.parse(PROBLEM_NAME);
-	
-	final int sup1Id = 1;
-	Supplier sup1 = problem.getSupplier(sup1Id);
-	final int sup2Id = 2;
-	Supplier sup2 = problem.getSupplier(sup2Id);
-	
 	/*
-	 * minore
+	 * sup1 < sup2
 	 */
 	@Test
 	public void testCompare1() {
+		final String PROBLEM_NAME = "problema4.txt";
+		Problem problem = pp.parse(PROBLEM_NAME);
+		final int sup1Id = 1;
+		Supplier sup1 = problem.getSupplier(sup1Id);
+		final int sup2Id = 2;
+		Supplier sup2 = problem.getSupplier(sup2Id);
+		
 		int s0[] = {0, 0, 0, 0};
 		int s1[] = {0, 51, 28, 0};
 		int s2[] = {0, 9, 12, 60};
@@ -40,22 +39,63 @@ public class CurrentPriceComparatorTest {
 		
 		final int product1 = 1;
 		CurrentPriceComparator comparator = new CurrentPriceComparator(sol, product1);
+		assertTrue(comparator.compare(sup1,sup2) < 0);
 	}
 	
 	/*
-	 * =
+	 * sup1 = sup2
 	 */
 	@Test
 	public void testCompare2() {
-		fail("Not yet implemented");
+		final String PROBLEM_NAME = "problema5.txt";
+		Problem problem = pp.parse(PROBLEM_NAME);
+		final int sup1Id = 1;
+		Supplier sup1 = problem.getSupplier(sup1Id);
+		final int sup2Id = 2;
+		Supplier sup2 = problem.getSupplier(sup2Id);
+		
+		int s0[] = {0, 0, 0, 0};
+		int s1[] = {0, 51, 28, 0};
+		int s2[] = {0, 9, 12, 60};
+		int matrix[][] = new int[3][];
+		matrix[0] = s0;
+		matrix[1] = s1;
+		matrix[2] = s2;
+		
+		Solution sol = new Solution(matrix, problem);
+		assertTrue(sol.isAdmissible(problem));
+		
+		final int product2 = 2;
+		CurrentPriceComparator comparator = new CurrentPriceComparator(sol, product2);
+		assertTrue(comparator.compare(sup1,sup2) == 0);	
 	}
 	
 	/*
-	 * >
+	 * sup1 > sup2
 	 */
 	@Test
 	public void testCompare3() {
-		fail("Not yet implemented");
+		final String PROBLEM_NAME = "problema5.txt";
+		Problem problem = pp.parse(PROBLEM_NAME);
+		final int sup1Id = 1;
+		Supplier sup1 = problem.getSupplier(sup1Id);
+		final int sup2Id = 2;
+		Supplier sup2 = problem.getSupplier(sup2Id);
+		
+		int s0[] = {0, 0, 0, 0};
+		int s1[] = {0, 51, 28, 0};
+		int s2[] = {0, 9, 12, 60};
+		int matrix[][] = new int[3][];
+		matrix[0] = s0;
+		matrix[1] = s1;
+		matrix[2] = s2;
+		
+		Solution sol = new Solution(matrix, problem);
+		assertTrue(sol.isAdmissible(problem));
+		
+		final int product3 = 3;
+		CurrentPriceComparator comparator = new CurrentPriceComparator(sol, product3);
+		assertTrue(comparator.compare(sup1,sup2) > 0);	
 	}
 
 }

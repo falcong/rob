63453d8a47508b521d4310935c1d7767be6c587b
claudio.landi24@gmail.com <claudio.landi24@gmail.com@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-07-26 12:06:09 +0000

    scritte prime 2 classi di test

--- trunk/rob/src/testingjunit/BanFullNeighbourGeneratorTest.java
+++ trunk/rob/src/testingjunit/BanFullNeighbourGeneratorTest.java
@@ -1,10 +1,18 @@
 package testingjunit;
 
 import static org.junit.Assert.*;
+import io.ProblemParser;
 
 import org.junit.Test;
 
+import rob.Problem;
+import rob.Solution;
+import rob.Utility;
+import solutionhandlers.BanFullNeighbourGenerator;
+import solutionhandlers.RandomSolutionGenerator;
+
 public class BanFullNeighbourGeneratorTest {
+	final String CLASS_NAME = this.getClass().getName();
 	
 	//test di generate()
 	/*
@@ -12,7 +20,59 @@ public class BanFullNeighbourGeneratorTest {
 	 */
 	@Test
 	public void testGenerate1() {
-		fail("Not yet implemented");
+		final String methodName = new Exception().getStackTrace()[0].getMethodName(); 
+		ProblemParser pp = new ProblemParser(Constants.INPUT_PATH);
+		
+		final String PROBLEM_NAME = "Cap.10.100.3.1.10.1.ctqd";
+		Problem problem = pp.parse(PROBLEM_NAME);
+		//primo numero nel nome del problema
+		final int NUM_SUPPLIERS = 10; 
+		
+		RandomSolutionGenerator randomGenerator = new RandomSolutionGenerator(problem);
+		//sol0 0 sol iniziale casuale
+		Solution sol0 = randomGenerator.generate();
+		assertTrue(sol0.isAdmissible(problem));
+		
+		BanFullNeighbourGenerator banGenerator = new BanFullNeighbourGenerator(problem); 
+		
+		//esegue il test N volte perché il metodo non è deterministico
+		final int N = 100;
+		for(int i=1; i<=N; i++){
+			//provo tutte le possibili distanze
+			final int MAX_DISTANCE = NUM_SUPPLIERS/2;
+			for(int distance = 1; distance<=MAX_DISTANCE; distance++){
+				Solution sol1 = banGenerator.generate(sol0, distance);
+				//controllo ammissibilità della soluzione generata dal metodo
+				boolean ok = sol1.isAdmissible(problem);
+				
+				
+				if(!ok){
+					System.out.println("fallimento del metodo con:\n"+
+							"num di test = "+i+"\n"+
+							"distance = "+distance);
+					final String FILE_NAME_SOL0 = CLASS_NAME+"_"+methodName+"_"+i+"_"+distance+"_sol0";
+					final String FILE_NAME_SOL1 = CLASS_NAME+"_"+methodName+"_"+i+"_"+distance+"_sol1";
+					sol0.export(FILE_NAME_SOL0);
+					sol1.export(FILE_NAME_SOL1);
+				}
+				
+				assertTrue(ok);
+			}
+		}
 	}
-
+	
+	
+	//TODO cancellare seguente!
+	//###########################################################################
+/*	@Test
+	public void testProva(){
+		//System.out.println(this.getClass().getName());
+		//System.out.println(CLASS_NAME);
+		
+		//System.out.println(new Exception().getStackTrace()[0].getMethodName());
+		
+		System.out.println("prima");
+		assertTrue(false);
+		System.out.println("ciao");
+	}*/
 }

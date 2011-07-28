f8ff4bcca90581459603d178e4e1d4371d747425
abiola.alowosile@gmail.com <abiola.alowosile@gmail.com@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-07-28 16:21:49 +0000

    modifica supplier

--- trunk/rob/src/testingjunit/SupplierTest.java
+++ trunk/rob/src/testingjunit/SupplierTest.java
@@ -11,97 +11,268 @@ import rob.Problem;
 import rob.Solution;
 import rob.Supplier;
 import rob.Utility;
+import solutionhandlers.RandomSolutionGenerator;
 
 public class SupplierTest {
+	ProblemParser pp = new ProblemParser(Constants.INPUT_PATH);
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	//test di activatedSegment()
+	//test di activatedSegment() 
+	//fasce di sconto con limiti 50 e 70
 	/*
-	 * 4 casi di test : es tre fascie di sconto con limiti 100 e 200
-	 * 50,99,100,250
+	 * Caso con totalQuantityBought < di 50
 	 */
 	@Test
 	public final void testActivatedSegment1(){
+		final String PROBLEM_NAME = "problema1_abiola.txt";
+		final int ACTIVATED_SEGMENT_EXPECTED = 0; 
+		testActivatedSegment(PROBLEM_NAME, ACTIVATED_SEGMENT_EXPECTED);
+	}
+	
+	/*
+	 * Caso con totalQuantityBought pari a 49 (Boundary test)
+	 */
+	@Test
+	public final void testActivatedSegment2(){
+		final String PROBLEM_NAME = "problema2_abiola.txt";
+		final int ACTIVATED_SEGMENT_EXPECTED = 0; 
+		testActivatedSegment(PROBLEM_NAME, ACTIVATED_SEGMENT_EXPECTED);
+	}
+
+	/*
+	 * Caso con totalQuantityBought pari a 50 (Boundary test)
+	 */
+	@Test
+	public final void testActivatedSegment3(){
+		final String PROBLEM_NAME = "problema3_abiola.txt";
+		final int ACTIVATED_SEGMENT_EXPECTED = 1; 
+		testActivatedSegment(PROBLEM_NAME, ACTIVATED_SEGMENT_EXPECTED);
+	}
+	
+	/*
+	 * Caso con totalQuantityBought > 70
+	 */
+	@Test
+	public final void testActivatedSegment4(){
+		final String PROBLEM_NAME = "problema4_abiola.txt";
+		final int ACTIVATED_SEGMENT_EXPECTED = 2; 
+		testActivatedSegment(PROBLEM_NAME, ACTIVATED_SEGMENT_EXPECTED);
+	}
+	
+	private final void testActivatedSegment(String problemName, int activatedSegmentExpected){
+		Problem problem = pp.parse(problemName);
 		
+		RandomSolutionGenerator generator = new RandomSolutionGenerator(problem);
+		Solution solution = generator.generate();
+		
+		//considero solo il fornitore1
+		final int SUPPLIER1_ID = 1;
+		Supplier supplier = problem.getSupplier(SUPPLIER1_ID);
+		int totalQuantityBought = solution.totalQuantityBought(SUPPLIER1_ID);
+		int activatedSegmentInSol = supplier.activatedSegment(totalQuantityBought);
+		
+		assertTrue(activatedSegmentExpected == activatedSegmentInSol);
 	}
 	
 	//test di checkAvailability()
 	/*
-	 * 3 casi di test : -1, 0, n
-	 * 
+	 * Caso in cui il prodotto non c'è
 	 */
 	@Test
 	public final void testCheckAvailability1(){
-		
+		final String PROBLEM_NAME = "problema5_abiola.txt";
+		testCheckAvailability(PROBLEM_NAME, false);
 	}
 	
-	//test di getDiscountedPrice
 	/*
-	 * 4 casi di test : es tre fascie di sconto con limiti 100 e 200
-	 * 50,99,100,250
+	 * Caso in cui il prodotto c'è con la disponibilità = 0
 	 */
 	@Test
-	public final void testGetDiscountedPrice1(){
+	public final void testCheckAvailability2(){
+		final String PROBLEM_NAME = "problema6_abiola.txt";
+		testCheckAvailability(PROBLEM_NAME, true);
+	}
+	
+	/*
+	 * Caso in cui il prodotto c'è con la disponibilità = n
+	 */
+	@Test
+	public final void testCheckAvailability3(){
+		final String PROBLEM_NAME = "problema7_abiola.txt";
+		testCheckAvailability(PROBLEM_NAME, true);
+	}
+	
+	private final void testCheckAvailability(String problemName, boolean bool){
+		Problem problem = pp.parse(problemName);
+		final int SUPPLIER1_ID = 1;
+		final int PRODUCT1_ID = 1;
 		
+		Supplier supplier = problem.getSupplier(SUPPLIER1_ID);
+		assertTrue(supplier.checkAvailability(PRODUCT1_ID) == bool);
+	}
+	
+	//test di getDiscountedPrice()
+	//fasce di sconto con limiti 50 e 70
+    /*4 casi di test : es tre fascie di sconto con limiti 100 e 200
+     *50,99,100,250
+     */
+	/*
+     *Caso in cui si ricade nella prima fascia di sconto
+     */
+	@Test
+	public final void testGetDiscountedPrice1(){
+		int [] s0={0, 0, 0, 0};
+		int [] s1={0, 20, 0, 0};
+		int [] s2={0, 40, 42, 63};
+		int [][] matrix=new int[3][];
+		matrix[0]=s0;
+		matrix[1]=s1;
+		matrix[2]=s2;
+		
+		testGetDiscountedPrice(matrix);
+	}
+	
+	
+	/*
+     *Caso in cui si ricade su un boundary (=49)
+     */
+	@Test
+	public final void testGetDiscountedPrice2(){
+		int [] s0={0, 0, 0, 0};
+		int [] s1={0, 19, 30, 0};
+		int [] s2={0, 41, 12, 63};
+		int [][] matrix=new int[3][];
+		matrix[0]=s0;
+		matrix[1]=s1;
+		matrix[2]=s2;
+		
+		testGetDiscountedPrice(matrix);
+	}
+	
+	/*
+     *Caso in cui si ricade dopo l'ultimo boundary (> 70)
+     */
+	@Test
+	public final void testGetDiscountedPrice3(){
+		int [] s0={0, 0, 0, 0};
+		int [] s1={0, 20, 30, 27};
+		int [] s2={0, 40, 12, 36};
+		int [][] matrix=new int[3][];
+		matrix[0]=s0;
+		matrix[1]=s1;
+		matrix[2]=s2;
+		
+		testGetDiscountedPrice(matrix);	
+	}
+	/*
+     *Caso in cui si ricade sul primo boundary (=50)
+     */
+	@Test
+	public final void testGetDiscountedPrice4(){
+		int [] s0={0, 0, 0, 0};
+		int [] s1={0, 20, 30, 0};
+		int [] s2={0, 40, 12, 63};
+		int [][] matrix=new int[3][];
+		matrix[0]=s0;
+		matrix[1]=s1;
+		matrix[2]=s2;
+		
+		testGetDiscountedPrice(matrix);	
+	}
+	private final void testGetDiscountedPrice(int matrix [][]){
+		final String PROBLEM_NAME = "problema8_abiola.txt";
+		Problem problem = pp.parse(PROBLEM_NAME);
+		Solution solution = new Solution(matrix,problem);
+		assertTrue(solution.isAdmissible(problem));
+		
+		final int SUPPLIER1_ID = 1;
+		final int PRODUCT1_ID = 1;
+		
+		Supplier supplier = problem.getSupplier(SUPPLIER1_ID);
+		int totalQuantityBought = solution.totalQuantityBought(SUPPLIER1_ID);
+		double discountedPriceProduct1 = supplier.getDiscountedPrice(PRODUCT1_ID, totalQuantityBought);	
+		double expectedDiscountedPrice = supplier.getPrice(PRODUCT1_ID, supplier.activatedSegment(solution));
+		final double TOLERANCE = 0.1;
+		
+		assertEquals(expectedDiscountedPrice, discountedPriceProduct1, TOLERANCE);
 	}
 	
 	//test di getResidual(int, int[][])
 	/*
-	 * caso generale
-	 */
+	 *Caso generale
+	 */ 
 	@Test
 	public final void testGetResidual1(){
+		final String PROBLEM_NAME = "problema8_abiola.txt";
+		Problem problem = pp.parse(PROBLEM_NAME);
 		
+		int [] s0={0, 0, 0, 0};
+		int [] s1={0, 20, 30, 0};
+		int [] s2={0, 40, 12, 63};
+		int [][] matrix=new int[3][];
+		matrix[0]=s0;
+		matrix[1]=s1;
+		matrix[2]=s2;
+		
+		Solution solution = new Solution(matrix,problem);
+		assertTrue(solution.isAdmissible(problem));
+		
+		final int SUPPLIER2_ID = 2;
+		final int PRODUCT2_ID = 2;
+		
+		Supplier supplier = problem.getSupplier(SUPPLIER2_ID);
+		int availability = supplier.getAvailability(PRODUCT2_ID);
+		
+		int resAvailabilityExpected = availability - matrix[SUPPLIER2_ID][PRODUCT2_ID];
+		int resAvailability = supplier.getResidual(PRODUCT2_ID, matrix);
+		
+		assertTrue(resAvailability == resAvailabilityExpected);
 	}
-	
+	 
 	//test di getResidual(int, Solution)
-	/*
-	 * caso generale
-	 */
+	
+	 /*caso generale
+	 
 	@Test
 	public final void testGetResidualSolution1(){
 		
 	}
-	
+	 /*
 	//test di getTotalResidualAvailability()
-	/*
+	
 	 * caso generale
-	 */
+	 
 	@Test
 	public final void testGetTotalResidualAvailability1(){
 		
 	}
 	
 	//test di quantityToIncreaseSegment()
-	/*
+	
 	 * 4 casi di test : es tre fascie di sconto con limiti 100 e 200
 	 * 50,99,100,250
-	 */
+	 
 	@Test
 	public final void testQuantityToIncreaseSegment1(){
 		
 	}
 	
 	//test di quantityToNotDecreaseSegment()
-	/*
+	
 	 * caso generico + soglia + soglia-1
-	 */
+	 
 	@Test
 	public final void testQuantityToNotDecreaseSegment1(){
 		
 	}
 	
 	//test di setPrices()
-	/*
+	
 	 * caso generale
-	 */
+	 
 	@Test
 	public final void testSetPrices1(){
 		
-	}
+	}*/
 	
 	
 	
@@ -114,9 +285,9 @@ public class SupplierTest {
 	
 	
 	
-
+//TODO da cancellare
 	
-	@Test
+	/*@Test
 	public final void testActivatedSegment() {
 		ProblemParser pp = new ProblemParser(Utility.getConfigParameter("problemsPath"));
 		Problem p = pp.parse("problema1.txt");
@@ -219,5 +390,5 @@ public class SupplierTest {
 		Solution solution2=new Solution(matrix,problem);
 		assertTrue(solution2.isAdmissible(problem));
 		assertEquals(1,s.quantityToNotDecreaseSegment(solution2));
-	}
+	}*/
 }

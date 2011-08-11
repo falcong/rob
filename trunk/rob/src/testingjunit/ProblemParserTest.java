b9eb0c4f346b8433ae2fc9df9a761009f5e9f30f
claudio.landi24@gmail.com <claudio.landi24@gmail.com@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-08-11 09:31:07 +0000

    revisione fino ProbleParserTest incluso

--- trunk/rob/src/testingjunit/ProblemParserTest.java
+++ trunk/rob/src/testingjunit/ProblemParserTest.java
@@ -2,13 +2,21 @@ package testingjunit;
 
 import static org.junit.Assert.*;
 import io.ProblemParser;
-
 import org.junit.Test;
-
 import data.Problem;
 
 
 public class ProblemParserTest {
+	Problem problem;
+	final int PRODUCT_1_ID = 1;
+	final int PRODUCT_2_ID = 2;
+	final int SUPPLIER1_ID = 1;
+	final int SUPPLIER2_ID = 2;
+	//PRICE<fornitore><prodotto>
+	final int PRICE11 = 101;
+	final int PRICE21 = 201;
+	final int PRICE22 = 102;
+
 
 	//metodo parse
 	/*
@@ -31,7 +39,7 @@ public class ProblemParserTest {
 	
 	private final void testParse(String problemName) throws Exception {
 		ProblemParser pp = new ProblemParser(Constants.INPUT_PATH);
-		Problem problem = pp.parse(problemName);
+		problem = pp.parse(problemName);
 		
 		//controllo che tutti i dati del problema siano corretti
 		//NAME
@@ -46,42 +54,59 @@ public class ProblemParserTest {
 		assertTrue(problem.getDimension() == DIMENSION);
 		
 		//DEMAND_SECTION
+		checkDemand(); 
+		
+		//OFFER_SECTION
+		checkOffer();
+		
+		//DISCOUNT_SECTION
+		checkDiscount();
+	}
+
+	
+	/*
+	 * Controlla la correttezza dei valori letti nella sezione DEMAND.
+	 */
+	private void checkDemand() {
 		final int NUM_PRODUCTS = 2;
 		final int PRODUCT1_DEMAND = 60;
 		final int PRODUCT2_DEMAND = 40;
-		assertTrue(problem.getNumProducts() == NUM_PRODUCTS);
-		final int product1Id = 1;
-		assertTrue(problem.getProductDemand(product1Id) == PRODUCT1_DEMAND);
-		final int product2Id = 2;
-		assertTrue(problem.getProductDemand(product2Id) == PRODUCT2_DEMAND);
+		assertEquals(problem.getNumProducts(), NUM_PRODUCTS);
+		assertEquals(problem.getProductDemand(PRODUCT_1_ID), PRODUCT1_DEMAND);
+		assertEquals(problem.getProductDemand(PRODUCT_2_ID), PRODUCT2_DEMAND);
+	}
+	
+	
+	/*
+	 * Controlla la correttezza dei valori letti nella sezione OFFER.
+	 */
+	private void checkOffer(){
+		//controllo i prezzi
+		assertEquals(PRICE11, (int)problem.getSupplier(SUPPLIER1_ID).getBasePrice(PRODUCT_1_ID));
+		assertEquals(PRICE22, (int)problem.getSupplier(SUPPLIER2_ID).getBasePrice(PRODUCT_2_ID));
 		
-		//OFFER_SECTION
-		//PRICE<fornitore><prodotto>
-		final int SUPPLIER1_ID = 1;
-		final int SUPPLIER2_ID = 2;
-		final int PRICE11 = 101;
-		final int PRICE21 = 201;
-		final int PRICE22 = 102;
-		assertEquals(PRICE11, (int)problem.getSupplier(SUPPLIER1_ID).getBasePrice(product1Id));
-		assertEquals(PRICE22, (int)problem.getSupplier(SUPPLIER2_ID).getBasePrice(product2Id));
-		
+		//controllo le disponibilità
 		//AVAILABILITY<fornitore><prodotto>
 		final int AVAILABILITY11 = 51;
-		final int AVAILABILITY12 = 52;
-		final int AVAILABILITY21 = 9;
 		final int AVAILABILITY22 = 42;
-		//TODO arrivati qui
-		assertEquals(PRICE11, (int)problem.getSupplier(SUPPLIER1_ID).getBasePrice(product1Id));
-		
-		//OFFER_SECTION
+		assertEquals(AVAILABILITY11, (int)problem.getSupplier(SUPPLIER1_ID).getAvailability(PRODUCT_1_ID));
+		assertEquals(AVAILABILITY22, (int)problem.getSupplier(SUPPLIER2_ID).getAvailability(PRODUCT_2_ID));
+	}
+	
+	
+	/*
+	 * Controlla la correttezza dei valori letti nella sezione DISCOUNT.
+	 */
+	private void checkDiscount(){
+		//controllo i lower bounds
 		//LOWER_BOUND<fornitore> della fascia di sconto 1
 		final int LOWER_BOUND1 = 50;
 		final int LOWER_BOUND2 = 10;
-
 		final int SEGMENT1_ID = 1;
 		assertTrue(problem.getSupplier(SUPPLIER1_ID).getLowerBounds()[SEGMENT1_ID] == LOWER_BOUND1);
 		assertTrue(problem.getSupplier(SUPPLIER2_ID).getLowerBounds()[SEGMENT1_ID] == LOWER_BOUND2);
 		
+		//controllo gli sconti
 		//DISCOUNT<fornitore><fascia>
 		final int DISCOUNT11 = 4;
 		final int DISCOUNT21 = 3;
@@ -89,7 +114,7 @@ public class ProblemParserTest {
 		double price111 = PRICE11*(100-DISCOUNT11)/100.0;
 		double price211 = PRICE21*(100-DISCOUNT21)/100.0;
 		final double TOLERANCE = 0.1;
-		assertEquals(price111, problem.getSupplier(SUPPLIER1_ID).getPrice(product1Id, SEGMENT1_ID), TOLERANCE);
-		assertEquals(price211, problem.getSupplier(SUPPLIER2_ID).getPrice(product1Id, SEGMENT1_ID), TOLERANCE);
+		assertEquals(price111, problem.getSupplier(SUPPLIER1_ID).getPrice(PRODUCT_1_ID, SEGMENT1_ID), TOLERANCE);
+		assertEquals(price211, problem.getSupplier(SUPPLIER2_ID).getPrice(PRODUCT_1_ID, SEGMENT1_ID), TOLERANCE);
 	}
 }

c23d65dc81851e2ac04dc2a2e61d5837250e2a32
nekomukuro <nekomukuro@03d01242-c3b1-f45a-3f34-d6bb31f178bf>
2011-08-14 15:16:19 +0000

    Finito package solvingalgorithm

--- trunk/rob/src/solvingalgorithm/temporizedalgorithm/localsearch/LocalSearch.java
+++ trunk/rob/src/solvingalgorithm/temporizedalgorithm/localsearch/LocalSearch.java
@@ -6,49 +6,87 @@ import data.Problem;
 import data.Solution;
 import solvingalgorithm.temporizedalgorithm.TemporizedAlgorithm;
 import solvingalgorithm.temporizedalgorithm.Timer;
-
+/**
+ * Questa classe implementa un algoritmo di ricerca locale. 
+ *
+ */
 public class LocalSearch extends TemporizedAlgorithm{
 	
-	ExplorationStrategy strategy;
+	private ExplorationStrategy strategy;
 	
+	/**
+	 * Questa enumerazione serve per impostare la strategia di esplorazione
+	 * degli intorni nella ricerca locale quando viene costruito l'oggetto LocalSearch.
+	 */
 	public enum StrategyName{
-		FIRST_IMPROVEMENT, BEST_IMPROVEMENT 
+		/**
+		 * Strategia First Improvement
+		 * @see FirstImprovementStrategy
+		 */
+		FIRST_IMPROVEMENT,
+		/**
+		 * Strategia Best Improvement
+		 * @see BestImprovementStrategy
+		 */
+		BEST_IMPROVEMENT 
 	}
 	
-	private int maxNeighboursNumber; //Numero massimo di vicini da esplorare
+	private int maxNeighboursNumber; 
 	private int maxStepsNumber; //numero massimo di passi prima di arrestare
 	private NeighbourGenerator generator;
 	
-	public LocalSearch(int maxNeighboursNumber, int maxStepsNumber, StrategyName successorChoice,
+	/**
+	 * Costruisce un oggetto LocalSearch.
+	 * @param maxNeighboursNumber - massimo numero di vicini da esaminare ad ogni passo
+	 * @param maxStepsNumber - massimo numero di passi prima di arrestare la ricerca
+	 * @param explorationStrategy - strategia di esplorazione dell'intorno, vedi {@link StrategyName}.
+	 * @param generator - algoritmo di generazione dei vicini.
+	 * @param problem - il problema da risolvere.
+	 */
+	public LocalSearch(int maxNeighboursNumber, int maxStepsNumber, StrategyName explorationStrategy,
 			NeighbourGenerator generator, Problem problem){
-		this.problem=problem;
-		this.maxNeighboursNumber=maxNeighboursNumber;
-		this.maxStepsNumber=maxStepsNumber;
-		//this.successorChoice=successorChoice;
-		this.generator= generator;
-		switch(successorChoice) {
+		this.problem = problem;
+		this.maxNeighboursNumber = maxNeighboursNumber;
+		this.maxStepsNumber = maxStepsNumber;
+		this.generator = generator;
+		
+		switch(explorationStrategy) {
 		case FIRST_IMPROVEMENT: strategy =new FirstImprovementStrategy();break;
 		case BEST_IMPROVEMENT: strategy =new BestImprovementStrategy();break;
 		}
 		
 	}
 	
-	//maximumTime espresso in secondi
+	/**
+	 * Costruisce un oggetto LocalSearch ricevendo in ingresso anche il tempo massimo di esecuzione.
+	 * @see #LocalSearch(int, int, StrategyName, NeighbourGenerator, Problem)
+	 * @see Timer
+	 * @param maxNeighboursNumber
+	 * @param maxStepsNumber
+	 * @param successorChoice
+	 * @param generator
+	 * @param problem
+	 * @param maxTime - tempo massimo di esecuzione espresso in secondi.
+	 */
 	public LocalSearch(int maxNeighboursNumber, int maxStepsNumber, StrategyName successorChoice,
 			NeighbourGenerator generator, Problem problem, int maxTime){
 		this(maxTime, maxTime, successorChoice, generator, problem);
 		timer = new Timer(maxTime);
 	}
 	
+	/**
+	 * Esegue la ricerca locale a partire da {@code startSolution}.
+	 * @return una soluzione la cui funzione obiettivo sia migliore o uguale a {@code startSolution}.
+	 */
 	public Solution execute(Solution startSolution){
-		if(timer!=null){
+		if(timer != null){
 			timer.addObserver(this);
 			startTimer();
 		}
 		
 		Solution currentSolution=startSolution;
 		int step;
-		for (step=1;step<=maxStepsNumber;step++) {
+		for (step=1; step<=maxStepsNumber; step++) {
 			//controllo che il timer non mi abbia detto di fermarmi
 			if(stop){
 				return currentSolution;
@@ -56,9 +94,9 @@ public class LocalSearch extends TemporizedAlgorithm{
 			
 			Solution successor;
 			successor = strategy.explore(currentSolution, maxNeighboursNumber, generator);
-			if(successor!=null){ 
+			if(successor != null){ 
 				//ho un successore, continuo la ricerca
-				currentSolution=successor;
+				currentSolution = successor;
 			}
 			else{
 				//l'esplorazione del vicinato ha ritornato null, siamo in un minimo locale
@@ -69,16 +107,3 @@ public class LocalSearch extends TemporizedAlgorithm{
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
